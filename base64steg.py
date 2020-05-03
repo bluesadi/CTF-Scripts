b9620e54487c8695e9b2d46a47d69d23dab018d2
bluesadi <liuyibo@stu.scu.edu.cn>
2020-05-03 15:55:56 +0800

    Base64隐写解密脚本

--- base64steg.py
+++ base64steg.py
@@ -1,9 +1,23 @@
+'''
+Base64隐写解密脚本
+'''
 import base64
- 
-def encrypt():
+
+BASE64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+def decrypt():
+    bin_str = ''
     with open('stego.txt') as file:
         for line in file:
-            line = line.replace('\n','0')
-            print(line)
+            line = line.replace('\n','')
+            equ_cnt = line.count('=')
+            if(equ_cnt):
+                last_chr = BASE64.index(line[len(line) - equ_cnt - 1])
+                bin_str += bin(last_chr).replace('0b','').zfill(8)[8 - 2 * equ_cnt:]
+    flag = ''
+    for i in range(0,len(bin_str),8):
+        flag += chr(int(bin_str[i:i + 8],2))
+    return flag
 
-encrypt()
+
+print(decrypt())
