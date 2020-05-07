2c0aab9bcec12f87fe64e3366956885512031af4
bluesadi <liuyibo@stu.scu.edu.cn>
2020-05-08 00:19:57 +0800

    RC4解密脚本

---
+++ RC4Decryption.py
@@ -0,0 +1,6 @@
+from Crypto.Cipher import ARC4
+
+data = b"\x9D\x87\x71\xA4\x83\x0B\xAA\x53\xC4\x38\x36\x85"
+key = b"a1dWT1pJXF1USVxRV1ZbUElQSVBJ"
+m = ARC4.new(key).decrypt(data)
+print(m.decode())
