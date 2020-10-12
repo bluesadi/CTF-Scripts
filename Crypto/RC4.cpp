be4d9e4dabdcb66743c3dd73d4589b3072dd63e9
bluesadi <liuyibo@stu.scu.edu.cn>
2020-10-12 19:18:17 +0800

    update RC4加解密 C/CPP实现

--- Crypto/RC4.cpp
+++ Crypto/RC4.cpp
@@ -1,45 +1,45 @@
 /*
-RC4加解密cpp实现
-参考资料：https://blog.csdn.net/cpongo3/article/details/89708316
+RC4加解密 C/CPP实现
+2020/10/12 by bluesadi
 */
-#include <bits/stdc++.h>
-using namespace std;
-#define byte unsigned char
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#define Byte unsigned char
+using std::swap;
 
-byte S[256],T[256];
+Byte S[256],T[256];
+char key[256] = "yyds";
+int keylen = strlen(key);
 
-void RC4Init(const byte *key,int keylen){
-    for(int i = 0;i < 256;i ++) S[i] = i;
-    for(int i = 0;i < 256;i ++) T[i] = key[i % keylen];
+void RC4Init(){
+    for(int i = 0;i < 256;i++) S[i] = i,T[i] = key[i % keylen];
     int j = 0;
-    for (int i = 0 ;i < 256 ;i ++){
+    for(int i = 0;i < 256;i++){
         j = (j + S[i] + T[i]) % 256;
-        swap(S[i] , S[j]);
+        swap(S[i],S[j]);
     }
 }
 
-void PRGA(byte *plaintext,int len){
-    int x = 0,y = 0;
-    for(int i = 0;i < len;i ++){
-        x = (x + 1) % 256;
-        y = (y + S[x]) % 256;
-        swap(S[x],S[y]);
-        int r = (S[x] + S[y]) % 256;
-        plaintext[i] ^= S[r];
+void RC4(Byte *dest,const char *src){
+    RC4Init();
+    int i = 0,j = 0;
+    for(int k = 0;src[k];k++){
+        i = (i + 1) % 256;
+        j = (j + S[i]) % 256;
+        swap(S[i],S[j]);
+        int t = (S[i] + S[j]) % 256;
+        dest[k] = src[k] ^ S[t];
     }
-}
-
-byte* RC4Encrypt(byte *plaintext,int len,const byte *key,int keylen){
-    byte* ciphertext = (byte*)malloc(keylen * sizeof(byte));
-    RC4Init(key,keylen);
-    PRGA(plaintext,len);
 }
 
 int main(){
-    byte plaintext[100] = "\x9D\x87\x71\xA4\x83\x0B\xAA\x53\xC4\x38\x36\x85";
-    int len = 12;
-    byte key[100] = "a1dWT1pJXF1USVxRV1ZbUElQSVBJ";
-    int keylen = 28;
-    RC4Encrypt(plaintext,12,key,28);
-    for(int i = 0;i < 12;i ++) putchar(plaintext[i]);
+    Byte *dest = new Byte[100];
+    memset(dest,0,100);
+    RC4(dest,"yyds");
+    printf("b'");
+    for(int i = 0;dest[i];i++){
+        printf("\\x%x",dest[i]);
+    }
+    printf("'");
 }
